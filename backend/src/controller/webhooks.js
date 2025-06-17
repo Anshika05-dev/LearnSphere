469643f5bb089ebea6f4455d4f201360eeba4abd
Anshika05-dev <anshika27.asr@gmail.com>
2025-06-17 19:17:52 +0530

    changes

--- backend/src/controller/webhooks.js
+++ backend/src/controller/webhooks.js
@@ -17,7 +17,7 @@ try {
         case 'user.created':{
             const userData={
                 _id:data.id,
-                email:data.email_address[0].email_address,
+                email:data.email_addresses[0].email_address,
                 name:data.first_name + " " + data.last_name,
                 imageUrl:data.image_url,
             }
@@ -27,7 +27,7 @@ try {
         }
         case 'user.updated':{
             const userData={
-                email:data.email_address[0].email_address,
+                email:data.email_addresses[0].email_address,
                 name:data.first_name + " " + data.last_name,
                 imageUrl:data.image_url,
             }
