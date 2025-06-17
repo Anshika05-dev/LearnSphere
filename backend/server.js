34397950ddecd33bbc7ac1a0c33a3513a81eb96a
Anshika05-dev <anshika27.asr@gmail.com>
2025-06-17 19:48:36 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,22 @@
-import express from 'express'
-import cors from 'cors'
-import 'dotenv/config'
-import connectDB from './src/config/mongodb.js'
-import { clerkWebhooks } from './src/controller/webhooks.js'
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+import connectDB from './src/config/mongodb.js';
+import { clerkWebhooks } from './src/controller/webhooks.js';
 
-const app=express()
-await connectDB()
-app.use(cors())
+const app = express();
+
+// connect to MongoDB
+await connectDB();
+
+app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res) => res.send("LMS API working..."));
 
-app.get('/',(req,res)=>res.send("LMS api working.."))
 app.post('/clerk', clerkWebhooks);
 
-const PORT=process.env.PORT||5002
-
-app.listen(PORT,()=>{
-  console.log(`Server running on port ${PORT}`)
-})
+const PORT = process.env.PORT || 5002;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
--- backend/src/config/mongodb.js
+++ backend/src/config/mongodb.js
@@ -1,6 +1,8 @@
-import mongoose from "mongoose";
-const connectDB=async()=>{
-    mongoose.connection.on('connected',()=>console.log('Connected to DataBase'))
-await mongoose.connect(`${process.env.MONGODB_URI}/LearnSphere`)
-}
-export default connectDB
+import mongoose from 'mongoose';
+
+const connectDB = async () => {
+  mongoose.connection.on('connected', () => console.log('Connected to DataBase'));
+  await mongoose.connect(`${process.env.MONGODB_URI}/LearnSphere`);
+};
+
+export default connectDB;
