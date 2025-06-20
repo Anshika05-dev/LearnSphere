00691d2fcb289e133b9e8266e347b5e3a9528c40
Anshika05-dev <anshika27.asr@gmail.com>
2025-06-20 15:37:29 +0530

    stripe added

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,13 @@ import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 import connectDB from './src/config/mongodb.js';
-import { clerkWebhooks } from './src/controller/webhooks.js';
+import { clerkWebhooks, stripeWebhooks } from './src/controller/webhooks.js';
 import educatorRouter from './src/routes/educatorRoutes.js';
 import { clerkMiddleware } from '@clerk/express';
 import connectCloudinary from './src/config/cloudinary.js';
 import courseRouter from './src/routes/courseRoute.js';
 import userRouter from './src/routes/userRoutes.js';
-import bodyParser from 'body-parser';
+
 
 const app = express();
 
@@ -17,14 +17,14 @@ await connectDB();
 await connectCloudinary();
 
 app.use(cors());
-app.use(express.json());
 app.use(clerkMiddleware())
 
 app.get('/', (req, res) => res.send("LMS API working..."));
-app.post('/clerk', clerkWebhooks);
-app.use('/api/educator',educatorRouter)
-app.use('/api/course',courseRouter)
-app.use('/api/user', userRouter)
+app.post('/clerk', express.json(), clerkWebhooks);
+app.use('/api/educator', express.json(), educatorRouter)
+app.use('/api/course', express.json(), courseRouter)
+app.use('/api/user', express.json(), userRouter)
+app.post('/stripe', express.raw({type: 'application/json'}), stripeWebhooks)
 
 const PORT = process.env.PORT || 5002;
 app.listen(PORT, () => {
