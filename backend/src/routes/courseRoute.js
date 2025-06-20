aac98d6e7e81e199895d045f2757a7c5a4082226
Anshika05-dev <anshika27.asr@gmail.com>
2025-06-20 16:35:40 +0530

    ..

--- backend/src/routes/courseRoute.js
+++ backend/src/routes/courseRoute.js
@@ -1,6 +1,5 @@
 import express from "express";
 import { getAllCourses, getCourseId } from "../controller/courseController.js";
-import { protectEducator } from "../middleware/authMiddleware.js";
 const courseRouter=express.Router()
 
 courseRouter.get('/all',getAllCourses)
